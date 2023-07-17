6a49fbf6d4d287fc3d0231d6d5397708f034ce36
Victor Volkov <vektorn1212@gmail.com>
2023-07-17 21:05:26 +0200

    update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,13 @@
 import pytest
 from src.item import Item
+from src.phone import Phone
 
 
 @pytest.fixture
 def data():
     return Item("Смартфон", 10000, 20)
+
+
+@pytest.fixture
+def data_for_phone():
+    return Phone("iPhone 14", 120_000, 5, 2)
